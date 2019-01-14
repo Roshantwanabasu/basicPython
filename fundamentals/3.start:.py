db77adb13afedeab4dab680d1984f37759d74f94
Roshantwanabasu <roshan.twanabasu98@gmail.com>
2019-01-14 11:53:30 +0545

    start index task

---
+++ fundamentals/3.start:.py
@@ -0,0 +1,11 @@
+student_name = "Colette"
+print(student_name[3:])
+
+#Task 3
+# [ ] print the second half of the long_word
+long_word = "Consequences"
+y = 0
+for x in long_word:
+    y += 1
+print(long_word[int(y/2):])
+# [ ] print a message that there is "No Parking" on index 0 or index 4 streets
