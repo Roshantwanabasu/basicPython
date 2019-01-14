992dd604596d8b49768f78a56ed7d88865b41531
Roshantwanabasu <roshan.twanabasu98@gmail.com>
2019-01-14 10:27:10 +0545

    upload task1 indexslicing

---
+++ fundamentals/1.indexSlicing.py
@@ -0,0 +1,19 @@
+student_name = "Roshan"
+print(student_name[2:5])
+
+print("index 2, 3 & 4 of student_name:", student_name[2] + student_name[3] + student_name[4])
+long_word = 'Acknowledgement'
+print(long_word[2:11])
+print(long_word[2:11], "is the 3rd char through the 11th char")
+print(long_word[2:11], "is the index 2, \"" + long_word[2] + "\",", "through index 10, \"" + long_word[10] + "\"")
+
+#Task 1
+#slice a string
+#start & stop index
+# [ ] slice long_word to print "act" and to print "tic"
+long_word = "characteristics"
+print("Slicing of the Charcteristics word\n",long_word[4:7])
+print(long_word[11]+long_word[12]+long_word[13])
+# [ ] slice long_word to print "sequence"
+long_word = "Consequences"
+print(long_word[3:11])
