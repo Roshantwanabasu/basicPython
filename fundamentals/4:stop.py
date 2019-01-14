6df08d067ce0ddb827031308e0c7ebbb214db234
Roshantwanabasu <roshan.twanabasu98@gmail.com>
2019-01-14 11:56:38 +0545

    stop index tasks

---
+++ fundamentals/4:stop.py
@@ -0,0 +1,11 @@
+student_name = "Colette"
+print(student_name[:3])
+
+
+#Task 2
+# [ ] print the first half of the long_word
+long_word = "Consequences"
+count = 0
+for x in long_word:
+    count += 1
+print(long_word[:int(count/2)])
