7b8f27ac65d403241a171ec04c4cdd53549ae25a
Roshantwanabasu <roshan.twanabasu98@gmail.com>
2019-01-15 14:06:40 +0545

    addition of destination for zip extraction

--- qtClasses/fileExtraction.py
+++ qtClasses/fileExtraction.py
@@ -1,3 +1,23 @@
+import argparse
+import tarfile
 import zipfile
-with zipfile.ZipFile("/home/roshan/Downloads/abc.zip", 'r') as zip_ref:
-    zip_ref.extractall("/home/roshan/Downloads/try")
+
+parser=argparse.ArgumentParser()
+parser.add_argument('--f', '--sourcefilePath', required=True, dest='filePath', type=str, help="file path to be extracted ")
+parser.add_argument("dest",type=str)
+args=parser.parse_args()
+filePath=args.filePath
+destpath=args.dest
+
+
+if (filePath.endswith("tar.gz")):
+    tar = tarfile.open(filePath, "r:gz")
+    tar.extractall(destpath)
+    tar.close()
+elif (filePath.endswith("tar")):
+    tar = tarfile.open(filePath, "r:")
+    tar.extractall(destpath)
+    tar.close()
+else:
+    zip = zipfile.ZipFile(filePath)
+    zip.extractall(destpath)
